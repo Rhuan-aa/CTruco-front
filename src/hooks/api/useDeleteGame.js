0a92df64e8b70e0f699fa51808f5e377f591b492
rhuanaa <rhuanandrey@gmail.com>
2025-12-09 17:16:18 -0300

    fix: deleteGame to invite logic

--- src/hooks/api/useDeleteGame.js
+++ src/hooks/api/useDeleteGame.js
@@ -1,18 +1,17 @@
 import useAuth from '../context/useAuth'
 import useIntel from '../context/useIntel'
-import useAxiosPrivate from './useAxiosPrivate'
+import useWebSocket from '../context/useWebSocket';
 
 const useDeleteGame = () => {
-    const axiosPrivate = useAxiosPrivate();
     const { auth: { uuid } } = useAuth()
-    const { setIntel } = useIntel()
+    const { intel } = useIntel()
+    const { sendMessage } = useWebSocket();
 
     const deleteConcludedGame = async () => {
         try {
-            
-            const url = `/api/v1/games/players/${uuid}`
-            await axiosPrivate.delete(url)
-            setIntel(null)
+            const gameId = intel.last.gameId;
+            const destination = `/api/v2/games/${gameId}/quit`
+            sendMessage(destination, uuid)
         }
         catch (error) {
             console.log(error.response.headers.authorization)
