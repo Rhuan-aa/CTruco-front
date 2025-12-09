d77673fe07e5cf5e9e786142fe566c83f4f22bca
rhuanaa <rhuanandrey@gmail.com>
2025-12-09 17:15:58 -0300

    fix: createGame to invite logic

--- src/hooks/api/useCreateGame.js
+++ src/hooks/api/useCreateGame.js
@@ -1,59 +1,29 @@
+import { useCallback } from "react";
 import useAuth from "../context/useAuth";
 import useIntel from "../context/useIntel";
 import useWebSocket from "../context/useWebSocket";
 import useAxiosPrivate from "./useAxiosPrivate";
+import useHandleGame from "./useHandleGame";
+import useHandleQuit from "./useHandleQuit";
 
 const useCreateGame = () => {
     const axiosPrivate = useAxiosPrivate();
     const { auth } = useAuth();
     const { setIntel } = useIntel();
     const { subscribe } = useWebSocket();
+    const handleGame = useHandleGame();
+    const handleQuit = useHandleQuit();
 
-    // async function processIntelSince(intelSince) {
-    //     console.log("Processando " + intelSince.length + " novos intel(s).");
-
-    //     if (intelSince.length === 0) return;
-
-    //     const mostRecent = intelSince.slice(-1)[0];
-
-    //     setIntel((prevState) => {
-    //         const lastIntel = prevState && prevState.last ? prevState.last : null;
-    //         const missing = lastIntel ? [lastIntel, ...intelSince] : intelSince;
-
-    //         return {
-    //             ...prevState,
-    //             last: mostRecent,
-    //             missing,
-    //         };
-    //     });
-    // }
-
-    // const handleGame = (message) => {
-    //     if (message.body === null) return;
-    //     try {
-    //         const data = JSON.parse(message.body);
-    //         const intelSince = data.intelSinceBaseTimestamp;
-    //         if (!intelSince || intelSince.length === 0) {
-    //             return;
-    //         }
-    //         processIntelSince(intelSince);
-    //     } catch (error) {
-    //         console.error("Erro ao processar mensagem do WebSocket:", error);
-    //     }
-    // };
-
-    const withBot = async (botName) => {
+    const withBot = useCallback(async (botName) => {
         try {
             const url = `/api/v1/games/user-bot/`;
             const data = { userUuid: auth.uuid, botName };
-
             const { data: initialIntel } = await axiosPrivate.post(url, data);
-
             setIntel({ last: initialIntel, opponentName: botName });
-
             const gameId = initialIntel.gameId;
         
-            subscribe(`/topic/game/${gameId}`);
+            subscribe(`/topic/game/${gameId}/state`, handleGame);
+            subscribe(`/topic/game/${gameId}/quit`, handleQuit);
 
             return initialIntel;
         } catch (error) {
@@ -61,8 +31,8 @@ const useCreateGame = () => {
                 error.response?.headers?.authorization || "Sem response disponível"
             );
         }
-    };
+    },[auth.uuid, axiosPrivate, setIntel, subscribe, handleGame, handleQuit]);
     return withBot;
-};
+}
 
 export default useCreateGame;
